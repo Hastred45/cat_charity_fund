2b321f467ec701fd348e1d98a8611c9ff328def0
Hastred45 <hastred45@yandex.ru>
2023-01-21 21:45:55 +0500

    final

--- app/core/config.py
+++ app/core/config.py
@@ -6,8 +6,8 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     app_title: str = 'Приложение QRKot'
     app_desc: str = 'Спасем котиков вместе!'
-    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
-    secret: str = 'SECRET'
+    database_url: str
+    secret: str
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
 
